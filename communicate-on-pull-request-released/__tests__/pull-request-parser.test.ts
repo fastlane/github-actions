97647d5909a9f451aa344f80f825c73835cf2480
Connor Tumbleson <iBotPeaches@users.noreply.github.com>
2026-01-17 08:47:34 -0500

    fix: support multiple issues in 1 pr (#171)
    * fix: support multiple issues in 1 pr
    
    * chore: restore old test

--- communicate-on-pull-request-released/__tests__/pull-request-parser.test.ts
+++ communicate-on-pull-request-released/__tests__/pull-request-parser.test.ts
@@ -67,6 +67,12 @@ describe('pull request parser test suite', () => {
       issueNumber: 15,
       owner: 'foo',
       repo: 'bar'
+    },
+    {
+      prBody: 'Fixes #456 and closes #456',
+      issueNumber: 456,
+      owner: 'foo',
+      repo: 'bar'
     }
   ];
 
@@ -120,13 +126,13 @@ describe('pull request parser test suite', () => {
 
   for (const scenario of validScenarios) {
     it(`It detects referenced issue for (${scenario.prBody})`, async () => {
-      expect(getReferencedIssue(scenario.owner, scenario.repo, scenario.prBody)).toEqual(scenario.issueNumber);
+      expect(getReferencedIssue(scenario.owner, scenario.repo, scenario.prBody)).toEqual([scenario.issueNumber]);
     });
   }
 
   for (const scenario of invalidScenarios) {
     it(`It does not detect referenced issue for (${scenario.prBody})`, async () => {
-      expect(getReferencedIssue(scenario.owner, scenario.repo, scenario.prBody)).toBeUndefined();
+      expect(getReferencedIssue(scenario.owner, scenario.repo, scenario.prBody)).toEqual([]);
     });
   }
 });
--- communicate-on-pull-request-released/src/pull-request-parser.ts
+++ communicate-on-pull-request-released/src/pull-request-parser.ts
@@ -14,35 +14,42 @@ let ISSUE_CLOSING_KEYWORDS = [
   'fix:'
 ];
 
-export function getReferencedIssue(owner: string, repo: string, prBody: string): number | undefined {
+export function getReferencedIssue(owner: string, repo: string, prBody: string): number[] {
   if (!prBody || prBody.trim() === '') {
-    return undefined;
+    return [];
   }
+
+  let issueNumbers: number[] = [];
+
   // Searching for issue closing keywords and issue identifier in pull request's description,
   // i.e. `fixes #1234`, `close #444`, `resolved #1`
-  let regex = new RegExp(`(${ISSUE_CLOSING_KEYWORDS.join('|')}) #\\d{1,}`, 'i');
+  let regex = new RegExp(`(${ISSUE_CLOSING_KEYWORDS.join('|')}) #\\d{1,}`, 'gi');
   let matched = prBody.match(regex);
   if (matched && matched.length > 0) {
-    const issueMatch = matched[0].match(/#\d{1,}/i);
-    if (issueMatch && issueMatch.length > 0) {
-      const issueNumber = issueMatch[0].replace('#', '');
-      return Number(issueNumber);
-    }
+    matched.forEach(match => {
+      const issueMatch = match.match(/#\d{1,}/i);
+      if (issueMatch && issueMatch.length > 0) {
+        const issueNumber = issueMatch[0].replace('#', '');
+        issueNumbers.push(Number(issueNumber));
+      }
+    });
   }
 
   // Searching for issue closing keywords and issue URL in pull request's description,
   // i.e. `closes https://github.com/REPOSITORY_OWNER/REPOSITORY_NAME/issues/1234`
   regex = new RegExp(
     `(${ISSUE_CLOSING_KEYWORDS.join('|')}) https:\\/\\/github.com\\/${owner}\\/${repo}\\/issues\\/\\d{1,}`,
-    'i'
+    'gi'
   );
   matched = prBody.match(regex);
   if (matched && matched.length > 0) {
-    const issue = matched[0].split('/').pop();
-    if (issue) {
-      return Number(issue);
-    }
+    matched.forEach(match => {
+      const issue = match.split('/').pop();
+      if (issue) {
+        issueNumbers.push(Number(issue));
+      }
+    });
   }
 
-  return undefined;
+  return [...new Set(issueNumbers)];
 }
