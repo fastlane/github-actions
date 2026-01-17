5bf9337d5e07e69e0e2e1ddfdcbe1f838ad200ef
Connor Tumbleson <connor.tumbleson@gmail.com>
2026-01-17 08:56:03 -0500

    fix: support blank pr labels for released/merged jobs

--- communicate-on-pull-request-released/jest.config.js
+++ communicate-on-pull-request-released/jest.config.js
@@ -11,6 +11,7 @@ export default {
   preset: 'ts-jest',
   reporters: ['default'],
   resolver: 'ts-jest-resolver',
+  resetModules: true,
   setupFilesAfterEnv: ['./__tests__/setup.js'],
   testEnvironment: 'node',
   testMatch: ['**/*.test.ts'],
